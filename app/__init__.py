222406b16d004cecb05c67c6cdadeae1a1d2a351
akankshasrivastava <project.akanksha@gmail.com>
2024-04-22 10:50:03 +0530

    User CRUD update

--- app/__init__.py
+++ app/__init__.py
@@ -4,36 +4,52 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+import os
 
-
+# Initialize Flask extensions
 db = SQLAlchemy()
 ma = Marshmallow()
 bcrypt = Bcrypt()
 
 
 def create_app():
+    # Create a Flask application instance
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1/Test'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 
+    # Database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI',
+                                                           'postgresql://postgres:postgres_lc@10.2.8.12/mweAnnotationPlatform')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Secret keys configuration
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_fallback_secret_key')
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY',
+                                                  'your_jwt_secret_key')  # Keep your existing JWT_SECRET_KEY
+
+    # Initialize Flask extensions with app context
     bcrypt.init_app(app)
     db.init_app(app)
     ma.init_app(app)
     jwt = JWTManager(app)
     CORS(app)
 
+    # Import and register blueprints
     with app.app_context():
         from .controllers.user_controller import user_blueprint
         from .controllers.project_controller import project_blueprint
         from .controllers.sentence_controller import sentence_blueprint
-        from .controllers.weather_controller import weather_blueprint
 
         app.register_blueprint(user_blueprint, url_prefix="/user")
         app.register_blueprint(project_blueprint, url_prefix="/project")
         app.register_blueprint(sentence_blueprint, url_prefix="/sentence")
-        app.register_blueprint(weather_blueprint, url_prefix="/weather")
 
+        # Create all database tables if they don't exist
         db.create_all()
 
     return app
+
+
+# If the file is executed as a standalone script, run the application
+if __name__ == '__main__':
+    app_instance = create_app()
+    app_instance.run(host='0.0.0.0', port=5000, debug=True)
