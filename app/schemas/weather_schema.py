69a4ec74a72ae98f6d36d3d8f185da24557a76e8
akankshasrivastava <project.akanksha@gmail.com>
2024-04-23 14:49:28 +0530

    .
    .

--- app/schemas/weather_schema.py
+++
@@ -1,8 +0,0 @@
-from .. import ma
-
-class WeatherSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'date', 'weather_main', 'weather_description','windspeed','temperature', 'humidity', 'location')
-
-weather_schema = WeatherSchema()
-weathers_schema = WeatherSchema(many=True)
